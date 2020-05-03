5c2c37c3fbf3042397eaedf3ba92da167c74c473
Greegko <greegko+dev@gmail.com>
2020-05-03 16:05:35 +0200

    improve activity type

--- packages/core/src/modules/activity/interfaces/activity.ts
+++ packages/core/src/modules/activity/interfaces/activity.ts
@@ -1,9 +1,14 @@
 export type ActivityID = string;
-export type AnyActivity = Activity<any>;
+export type AnyActivity = Activity<unknown>;
 export enum ActivityType { Party = 'party' };
-export type Activity<T> = {
+export type Activity<T = unknown> = {
   id: ActivityID;
   state: T;
   name: string;
   type: ActivityType;
+}
+
+export type PartyActivity<T = unknown> = Activity<T> & {
+  state: T & { partyId: string };
+  type: ActivityType.Party;
 }
