db42d6510441469e6c13d536f9ed3976ca8b91fe
Greegko <greegko+dev@gmail.com>
2020-09-27 23:56:53 +0200

    add types

--- packages/web/src/game/selectors/game.ts
+++ packages/web/src/game/selectors/game.ts
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 import { values, filter, groupBy } from 'ramda';
-import { isHero, ActivityType, GameState, PartyOwner } from "@rpg-village/core";
+import { isHero, ActivityType, GameState, PartyOwner, Activity, Party } from "@rpg-village/core";
 
 export const worldSelector = (game: GameState) => game.world;
 export const worldLocationsSelector = createSelector(worldSelector, values);
@@ -15,6 +15,6 @@ export const playerPartiesSelector = createSelector(partiesSelector, parties => 
 export const partiesGroupedOnLocationsSelector = createSelector(partiesSelector, parties => groupBy(party => party.locationId, values(parties)));
 
 export const activitiesSelector = (game: GameState) => game.activities;
-export const partyActivitiesSelector = createSelector(activitiesSelector, filter(activity => activity.type === ActivityType.Party));
+export const partyActivitiesSelector = createSelector(activitiesSelector, filter((activity: Activity) => activity.type === ActivityType.Party));
 
-export const idlePartiesSelector = createSelector(partiesSelector, filter(party => party.activityId === undefined));
+export const idlePartiesSelector = createSelector(partiesSelector, filter((party: Party) => party.activityId === undefined));
