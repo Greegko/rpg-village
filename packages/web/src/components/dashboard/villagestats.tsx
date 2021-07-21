4eb0714456aeff60cdb026d88b56ca9e6827894e
Greegko <greegko+dev@gmail.com>
2021-07-21 09:56:10 +0200

    generate gold command

--- packages/web/src/components/dashboard/villagestats.tsx
+++ packages/web/src/components/dashboard/villagestats.tsx
@@ -1,6 +1,8 @@
 import { connect } from "react-redux";
-import { VillageState, GeneralGameStoreState } from "@rpg-village/core";
-import { GameStoreState } from "../../game";
+import { VillageState, GeneralGameStoreState, VillageCommand } from "@rpg-village/core";
+import { GameStoreState, executeCommand } from "../../game";
+
+const storeDispatchers = { executeCommand };
 
 const propertyMapper = (state: GameStoreState) => {
   return {
@@ -12,14 +14,23 @@ const propertyMapper = (state: GameStoreState) => {
 interface VillageStatsProperties {
   village: VillageState;
   general: GeneralGameStoreState;
+  executeCommand: typeof executeCommand;
 }
 
 import "./villagestats.scss";
-export const VillageStats = connect(propertyMapper)(({ village, general }: VillageStatsProperties) => (
+export const VillageStats = connect(
+  propertyMapper,
+  storeDispatchers,
+)(({ village, general, executeCommand }: VillageStatsProperties) => (
   <div className="villagestats">
     <ul className="villagestats__list">
       <li className="villagestats__list-item">Turn: {general.turn}</li>
       <li className="villagestats__list-item">Gold: {village.stash.resource.gold}</li>
+      <li className="villagestats__list-item">
+        <button style={{ margin: 0 }} onClick={() => executeCommand({ command: VillageCommand.GenerateGold })}>
+          Generate Gold
+        </button>
+      </li>
     </ul>
   </div>
 ));
