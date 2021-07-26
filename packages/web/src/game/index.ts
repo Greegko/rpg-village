89215f7d4c01fa4ffb1422cb7f0854c8abf8bb33
Greegko <greegko+dev@gmail.com>
2021-07-26 09:52:19 +0200

    feat(web/core): add translation

--- packages/web/src/game/index.ts
+++ packages/web/src/game/index.ts
@@ -2,3 +2,4 @@ export * from "./interface";
 export * from "./reducers";
 export * from "./selectors";
 export * from "./actions";
+export * from "./translate";
---
+++ packages/web/src/game/translate.ts
@@ -0,0 +1,26 @@
+import { split, path, useWith } from "ramda";
+import { AttackEffectType, DefenseEffectType } from "@rpg-village/core";
+import { Path, PathValue } from "./dot-notation";
+
+const dotPath = useWith(path, [split(".")]);
+
+export const translation = {
+  core: {
+    model: {
+      attackEffectType: {
+        [AttackEffectType.Dmg]: "damage",
+        [AttackEffectType.CriticalChance]: "critical chance",
+      },
+      defenseEffectType: {
+        [DefenseEffectType.Armor]: "armor",
+        [DefenseEffectType.Evasion]: "evasion",
+      },
+    },
+  },
+} as const;
+
+type Translation = typeof translation;
+
+export function translate<P extends Path<Translation>>(path: P): PathValue<Translation, P> {
+  return dotPath(path, translation);
+}
