c0fbf9eb4c8de52f254d95910fb62b49b9e66743
Greegko <greegko+dev@gmail.com>
2021-12-12 16:35:34 +0100

    add only one testcase debugger option

--- packages/core/ava.config-debug.mjs
+++ packages/core/ava.config-debug.mjs
@@ -1,4 +1,26 @@
-export default {
+import * as fs from 'fs';
+
+const defaultSettings = {
   extensions: ["ts"],
   require: ["ts-node/register", "tsconfig-paths/register"],
 };
+
+const extraSettings = {};
+
+const file = process.argv[2]; // ${file}
+const fileLine = process.argv[6]; // --file-line = ${lineNumber}
+
+const fileLineText = fs.readFileSync(file, 'utf-8').split('\n')[fileLine - 1];
+const testNameFinderRegexp = RegExp("^test\\(\"(.+)\", \\{$");
+const match = fileLineText.match(testNameFinderRegexp);
+
+console.log();
+
+if(match) {
+  console.log('Running a specific testcase! =>', match[1])
+  extraSettings['match'] = match[1];
+} else {
+  console.log('Running all testcases');
+}
+
+export default { ...defaultSettings, ...extraSettings };
