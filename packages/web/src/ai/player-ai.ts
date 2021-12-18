1b789b4891dd51c5301e3aa9236fe5334b8c0639
Greegko <greegko+dev@gmail.com>
2021-12-18 17:26:42 +0100

    AI: update tile selection

--- packages/web/src/ai/player-ai.ts
+++ packages/web/src/ai/player-ai.ts
@@ -7,6 +7,7 @@ import {
   GameState,
   VillageCommand,
   calculateUnitStrength,
+  MapLocation,
 } from "@rpg-village/core";
 import { sample } from "../lib";
 import {
@@ -31,29 +32,17 @@ export class PlayerAI {
 
   private handleNewLocation(gameState: GameState, party: Party): Command | undefined {
     const world = worldSelector(gameState);
-    const parties = partiesSelector(gameState);
-    const units = unitsSelector(gameState);
     const partyLocation = world[party.locationId];
 
     if (!partyLocation.explored) {
       return { command: WorldCommand.Explore, args: { partyId: party.id } };
     }
 
-    const partiesOnLocation = values(parties).filter(x => x.locationId === partyLocation.id);
+    const [userParties, enemyParties] = this.getPartiesOnLocation(gameState, partyLocation.id);
 
-    if (partiesOnLocation.length !== 2) return;
+    if (userParties.length !== 1 || enemyParties.length !== 1) return;
 
-    const [[userParty], [enemyParty]] = partition(
-      party => party.owner === PartyOwner.Player,
-      values(parties).filter(x => x.locationId === partyLocation.id),
-    ) as [[Party], [Party]];
-
-    const userPartyUnits = userParty.unitIds.map(unitId => units[unitId]);
-    const enempyPartyUnits = enemyParty.unitIds.map(unitId => units[unitId]);
-
-    console.log(sum(userPartyUnits.map(calculateUnitStrength)), sum(enempyPartyUnits.map(calculateUnitStrength)));
-
-    if (sum(userPartyUnits.map(calculateUnitStrength)) > sum(enempyPartyUnits.map(calculateUnitStrength))) {
+    if (this.getPartyStrength(gameState, userParties[0]!.id) > this.getPartyStrength(gameState, enemyParties[0]!.id)) {
       return { command: WorldCommand.Battle, args: { locationId: partyLocation.id } };
     }
   }
@@ -71,13 +60,62 @@ export class PlayerAI {
       }
     }
 
-    const unexploredLocations = values(world).filter(location => !location.explored);
-    const newUnexploredLocation = sample(unexploredLocations);
+    const partyStrength = this.getPartyStrength(gameState, party.id);
+
+    const unexploredLocations = values(world).filter(location => !location.explored) as MapLocation[];
+    const weakerUnitLocations = values(world).filter(location => {
+      const [, [enemyParty]] = this.getPartiesOnLocation(gameState, location.id);
+
+      if (!enemyParty) return false;
+
+      const enemyPartyStrength = this.getPartyStrength(gameState, enemyParty.id);
+
+      return partyStrength > enemyPartyStrength;
+    }) as MapLocation[];
+
+    const possibleLocations = [...unexploredLocations, ...weakerUnitLocations].filter(location =>
+      this.isLocationAccessible(gameState, location.id),
+    );
+
+    const newUnexploredLocation = sample(possibleLocations);
 
     if (newUnexploredLocation) {
       return { command: WorldCommand.Travel, args: { partyId: party.id, targetLocationId: newUnexploredLocation.id } };
     } else {
       return { command: WorldCommand.Travel, args: { partyId: party.id, targetLocationId: village.locationId } };
     }
+  }
+
+  private getPartiesOnLocation(gameState: GameState, locationId: string): [Party[], Party[]] {
+    const parties = partiesSelector(gameState);
+
+    return partition(
+      party => party.owner === PartyOwner.Player,
+      values(parties).filter(x => x.locationId === locationId),
+    ) as [Party[], Party[]];
+  }
+
+  private getPartyStrength(gameState: GameState, partyId: string) {
+    const parties = partiesSelector(gameState);
+    const units = unitsSelector(gameState);
+
+    const partyUnits = (parties[partyId] as Party).unitIds.filter(unitId => units[unitId]).map(unitId => units[unitId]);
+
+    return sum(partyUnits.map(calculateUnitStrength));
+  }
+
+  private isLocationAccessible(gameState: GameState, locationId: string): boolean {
+    const locations = worldSelector(gameState);
+    const targetLocation = locations[locationId] as MapLocation;
+
+    const neighboursTiles = (values(locations) as MapLocation[]).filter(
+      location =>
+        Math.abs(location.x - targetLocation.x) <= 1 &&
+        location.x !== targetLocation.x &&
+        Math.abs(location.y - targetLocation.y) <= 1 &&
+        location.y !== targetLocation.y,
+    );
+
+    return neighboursTiles.some(location => this.getPartiesOnLocation(gameState, location.id)[1].length === 0);
   }
 }
