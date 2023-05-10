267859908fabce7b86f49f810edf1713c6e173f6
Greegko <greegko+github@gmail.com>
2023-05-10 20:39:39 +0200

    display boss location

--- packages/web/src/components/world-map/map/tile.tsx
+++ packages/web/src/components/world-map/map/tile.tsx
@@ -15,18 +15,16 @@ export const Tile = ({ x, y, parties, locationType }: TileProperties) => {
       <RegularPolygon sides={6} radius={40} rotation={90} x={x} y={y} fill="red" />
       {locationType === MapLocationType.Village && <Circle radius={18} fill="green" x={x} y={y} />}
       {locationType === MapLocationType.Portal && <Circle radius={18} fill="yellow" x={x} y={y} />}
-      {parties && parties[0] && parties[0].owner === PartyOwner.Player && (
-        <Circle radius={14} fill="blue" x={x} y={y} />
-      )}
-      {parties && parties[0] && parties[0].owner === PartyOwner.Enemy && (
-        <Circle radius={14} fill="black" x={x} y={y} />
-      )}
-      {parties && parties[1] && parties[1].owner === PartyOwner.Player && (
-        <Circle radius={14} fill="blue" x={x + 4} y={y + 4} />
-      )}
-      {parties && parties[1] && parties[1].owner === PartyOwner.Enemy && (
-        <Circle radius={14} fill="black" x={x + 4} y={y + 4} />
-      )}
+      {locationType === MapLocationType.Boss && <Circle radius={18} fill="purple" x={x} y={y} />}
+      {parties && parties.map((party, index) => <Party key={party.id} party={party} x={x + index * 4} y={y + index * 4} /> )}
     </Group>
   );
 };
+
+interface PartyProps { party: Party; x: number; y: number };
+const Party = ({ party, x, y }: PartyProps) => {
+  if(party.owner === PartyOwner.Player) return <Circle radius={14} fill="blue" x={x} y={y} />;
+  if(party.owner === PartyOwner.Enemy) return <Circle radius={14} fill="black" x={x} y={y} />;
+
+  return null;
+}
