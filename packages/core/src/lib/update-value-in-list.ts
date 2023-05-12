0331849499006e34c94c760d5a80e2640eaec7bd
Greegko <greegko+github@gmail.com>
2023-05-12 23:24:49 +0400

    fix updater

--- packages/core/src/lib/update-value-in-list.ts
+++ packages/core/src/lib/update-value-in-list.ts
@@ -4,7 +4,7 @@ export function updateValueInList<T>(list: T[], targetValue: T, updater: (val: T
   const newTargetValue = updater(targetValue);
 
   if (newTargetValue === null) {
-    return without(list, [targetValue]);
+    return without([targetValue], list);
   } else {
     return list.map(x => (x === targetValue ? newTargetValue : x));
   }
