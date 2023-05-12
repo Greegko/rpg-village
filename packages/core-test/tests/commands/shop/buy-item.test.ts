3d90d9ddf2a81fd82e51bc3248e171effe48bd92
Greegko <greegko+github@gmail.com>
2023-05-12 21:00:24 +0400

    add tests

--- packages/core-test/tests/commands/shop/buy-item.test.ts
+++ packages/core-test/tests/commands/shop/buy-item.test.ts
@@ -1,4 +1,4 @@
-import { ShopCommand } from "@rpg-village/core/dist/src/modules/shop/interfaces/village-command";
+import { ShopCommand } from "@rpg-village/core";
 
 import { createState, test } from "../../utils";
 
@@ -12,6 +12,34 @@ test("should remove resoure when village has enough resource", {
   ]),
   commands: [{ command: ShopCommand.BuyItem, args: { shopId: "shop-id", shopItemId: "shop-item-id" } }],
   expectedState: { village: { stash: { resource: { gold: 50 } } } },
+});
+
+test("should decrease quantity", {
+  initState: createState(({ shop, village }) => [
+    shop({
+      id: "shop-id",
+      items: [{ id: "shop-item-id", price: { gold: 50 }, quantity: 2, item: { name: "shop-buy-item" } }],
+    }),
+    village({ stash: { resource: { gold: 100 }, items: [] } }),
+  ]),
+  commands: [{ command: ShopCommand.BuyItem, args: { shopId: "shop-id", shopItemId: "shop-item-id" } }],
+  expectedState: (state, t) => t.is(state.shops["shop-id"].items.find(x => x.id === "shop-item-id").quantity, 1),
+});
+
+test("should remove item when runs out", {
+  initState: createState(({ shop, village }) => [
+    shop({
+      id: "shop-id",
+      items: [{ id: "shop-item-id", price: { gold: 50 }, quantity: 1, item: {} }],
+    }),
+    village({ stash: { resource: { gold: 100 }, items: [] } }),
+  ]),
+  commands: [{ command: ShopCommand.BuyItem, args: { shopId: "shop-id", shopItemId: "shop-item-id" } }],
+  expectedState: (state, t) =>
+    t.is(
+      state.shops["shop-id"].items.find(x => x.id === "shop-item-id"),
+      undefined,
+    ),
 });
 
 test("should put into village items", {
