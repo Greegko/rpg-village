075ef21a684182603084085e13d8019e2c05d6b3
Greegko <greegko+github@gmail.com>
2023-05-12 23:25:07 +0400

    add shop display and buy option

--- packages/web/src/components/dashboard/village-shop.tsx
+++ packages/web/src/components/dashboard/village-shop.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+
+import { Item, ShopCommand } from "@rpg-village/core";
+
+import { useGameExecuteCommand } from "@web/react-hooks";
 import { useGameStateSelector, villageShopSelector } from "@web/store/game";
 
 import { ItemList } from "./item-list";
 
 export const VillageShopPage = () => {
+  const execute = useGameExecuteCommand();
+  const [selectedItem, setSelectedItem] = useState<Item | null>();
   const shop = useGameStateSelector(villageShopSelector);
 
   if (!shop) return <div>No shop!</div>;
 
-  return <ItemList items={shop.items.map(x => x.item)} listSize={30} />;
+  return (
+    <>
+      {selectedItem && (
+        <button
+          onClick={() =>
+            execute({
+              command: ShopCommand.BuyItem,
+              args: { shopId: shop.id, shopItemId: shop.items.find(x => x.item === selectedItem)?.id! },
+            })
+          }
+        >
+          Buy
+        </button>
+      )}
+      <ItemList onItemSelect={setSelectedItem} items={shop.items.map(x => x.item)} listSize={30} />
+    </>
+  );
 };
