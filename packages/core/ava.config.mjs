86cb1a399b37f1a5d25ecad61b4f2d7754631c80
Greegko <greegko+github@gmail.com>
2023-08-03 12:09:36 +0200

    adjust to support esm core tests

--- packages/core/ava.config.mjs
+++ packages/core/ava.config.mjs
@@ -2,4 +2,5 @@ export default {
   ignoredByWatcher: ["tests/**/*", "src/**/*", "**/*.tsbuildinfo"],
   require: "reflect-metadata",
   files: ["dist/**/*.test.js"],
+  nodeArguments: ["--experimental-specifier-resolution=node", "--no-warnings"],
 };
