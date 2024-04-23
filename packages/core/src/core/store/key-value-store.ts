49e4fbee11a147030d4b2280b3701046f7d1d00a
Greegko <greegko+github@gmail.com>
2024-04-23 19:00:59 +0200

    simplify key value store

--- packages/core/src/core/store/key-value-store.ts
+++ packages/core/src/core/store/key-value-store.ts
@@ -1,4 +1,5 @@
 import { injectable } from "inversify";
+import { assoc, dissoc } from "rambda";
 
 type ValueUpdaterCallback<T> = (value: T | undefined) => T;
 type ValueUpdater<T> = T | ValueUpdaterCallback<T>;
@@ -9,31 +10,31 @@ function isValueUpdaterCallback<T>(valueOrUpdater: ValueUpdater<T>): valueOrUpda
 
 @injectable()
 export abstract class KeyValueStore<Key extends string, Value> {
-  private state: Map<Key, Value> = new Map();
+  private state = {} as Record<Key, Value>;
 
   getState(): Record<Key, Value> {
-    return Object.fromEntries(this.state.entries()) as Record<Key, Value>;
+    return this.state;
   }
 
   init(state: Record<Key, Value>) {
-    this.state = new Map<Key, Value>(Object.entries(state) as [Key, Value][]);
+    this.state = state;
   }
 
   get(id: Key): Value | undefined {
-    return this.state.get(id);
+    return this.state[id];
   }
 
   set(key: Key, updater: ValueUpdaterCallback<Value>): void;
   set(key: Key, value: Value): void;
   set(key: Key, valueOrUpdater: ValueUpdater<Value>): void {
     if (isValueUpdaterCallback(valueOrUpdater)) {
-      this.state.set(key, valueOrUpdater(this.get(key)));
+      this.state = assoc(key, valueOrUpdater(this.get(key)), this.state);
     } else {
-      this.state.set(key, valueOrUpdater as Value);
+      this.state = assoc(key, valueOrUpdater, this.state);
     }
   }
 
   remove(key: Key): void {
-    this.state.delete(key);
+    this.state = dissoc(key, this.state) as Record<Key, Value>;
   }
 }
