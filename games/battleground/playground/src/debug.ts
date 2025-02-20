caf03fc5e396cfd48a253aee259a8b9f8fe81bfd
Greegko <greegko+github@gmail.com>
2025-02-20 13:30:56 +0100

    use pixi.ticker

--- games/battleground/playground/src/debug.ts
+++ games/battleground/playground/src/debug.ts
@@ -1,8 +1,29 @@
-import { Loop } from "./loop";
+import { without } from "rambda";
+
+type Callback = Function;
+
+interface Listerens {
+  start: Callback[];
+  stop: Callback[];
+  tick: Callback[];
+}
 
 export class Debug {
-  constructor(private loop: Loop) {}
-  isRunning = true;
+  private isRunning = true;
+
+  private listeners: Listerens = {
+    start: [],
+    stop: [],
+    tick: [],
+  };
+
+  addEventListener(type: keyof Listerens, callback: () => void) {
+    this.listeners[type].push(callback);
+  }
+
+  removeEventListener(type: keyof Listerens, callback: () => void) {
+    this.listeners[type] = without(this.listeners[type], [callback]);
+  }
 
   hookGlobalKeys() {
     window.addEventListener("keypress", event => this.nextFrameEvent(event));
@@ -13,9 +34,9 @@ export class Debug {
     if (event.key !== " ") return;
 
     if (this.isRunning) {
-      this.loop.stop();
+      this.listeners.stop.forEach(fn => fn());
     } else {
-      this.loop.start();
+      this.listeners.start.forEach(fn => fn());
     }
 
     this.isRunning = !this.isRunning;
@@ -25,6 +46,6 @@ export class Debug {
     if (event.key !== "n") return;
     if (this.isRunning) return;
 
-    this.loop.tick();
+    this.listeners.tick.forEach(fn => fn());
   }
 }
--- games/battleground/playground/src/game.tsx
+++ games/battleground/playground/src/game.tsx
@@ -1,6 +1,8 @@
 import { decompressFromBase64 } from "lz-string";
 import { Show, createSignal, onMount } from "solid-js";
 
+import { Ticker } from "pixi.js";
+
 import {
   AssetManager,
   Battlefield,
@@ -20,6 +22,9 @@ export const Game = () => {
   const [spellSelection, setSpellSelection] = createSignal<SpellSelection>();
   const [assetManager, setAssetManager] = createSignal<AssetManager>();
   const [battlegroundCanvas, setBattlegroundCanvas] = createSignal<HTMLCanvasElement>();
+
+  const ticker = new Ticker();
+  ticker.maxFPS = 60;
 
   const [fps, setFPS] = createSignal(0);
   const [tick, setTick] = createSignal(0);
@@ -56,9 +61,8 @@ export const Game = () => {
       }
     })();
 
-    loop.setCallback((tick, fps) => {
-      setTick(tick);
-      setFPS(fps);
+    const doTick = () => {
+      setTick(tick => tick + 2);
 
       battleField.tick();
 
@@ -69,9 +73,20 @@ export const Game = () => {
       if (battleField.isFinished) {
         loop.stop();
       }
+    };
+
+    ticker.add(({ FPS }) => {
+      setFPS(Math.floor(FPS));
+      doTick();
     });
 
-    const debug = new Debug(loop);
+    ticker.start();
+
+    const debug = new Debug();
+
+    debug.addEventListener("start", () => ticker.start());
+    debug.addEventListener("stop", () => ticker.stop());
+    debug.addEventListener("tick", () => doTick());
 
     debug.hookGlobalKeys();
 
