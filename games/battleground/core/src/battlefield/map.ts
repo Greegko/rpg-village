85db7192eb1fd5c625141cf58df9e7045fa730ae
Greegko <greegko+github@gmail.com>
2025-02-21 13:12:13 +0100

    apply projectile type

--- games/battleground/core/src/battlefield/map.ts
+++ games/battleground/core/src/battlefield/map.ts
@@ -1,32 +1,54 @@
 import { without } from "rambda";
 
+import { Rectangle, SpatialHash } from "@rpg-village/utils";
+
 import { multVector, normVector, subVector } from "../utils";
 import { Context } from "./context";
-import { ProjectileNode } from "./interface";
+import { Position, ProjectileNode, ProjectileType } from "./interface";
 import { filterBySeekConditions } from "./utils/unit-filter";
+
+type MapNode = { id: string; location: Position; size: number };
+
+const nodeToRectangle = (node: MapNode) => ({
+  left: node.location.x,
+  right: node.location.x + node.size,
+  top: node.location.y,
+  bottom: node.location.y + node.size,
+});
+
+const projectileToRectangle = (node: ProjectileNode): Rectangle => ({
+  left: node.location.x,
+  right: node.location.x + 8,
+  top: node.location.y,
+  bottom: node.location.y + 8,
+});
 
 export class MapContext {
   constructor(private context: Context) {}
 
   projectiles: ProjectileNode[] = [];
 
-  tickUnitsMove(): void {
-    const aliveUnits = this.context.unit.units.filter(x => x.hp > 0);
-
-    for (let unit of aliveUnits.filter(x => x.moveDirection)) {
-      this.context.unit.separation(unit, aliveUnits);
-      this.context.unit.screenBoundaries(unit);
-      this.context.unit.moveUnit(unit);
-    }
-  }
-
   tickProjectiles(): void {
+    const hash = new SpatialHash<MapNode>(48, nodeToRectangle);
+    this.context.unit.units.forEach(unit => hash.set(unit));
+
     for (let projectile of this.projectiles) {
       projectile.timeState -= 1;
 
       const d = multVector(normVector(subVector(projectile.targetLocation, projectile.sourceLocation)), projectile.speed);
       projectile.location.x += d.x;
       projectile.location.y += d.y;
+
+      if (projectile.projectileType === ProjectileType.Linear) {
+        const set = hash.search(projectileToRectangle(projectile));
+        set.delete(projectile.source);
+
+        if (set.size > 0) {
+          this.landProjectile(projectile);
+          this.projectiles = without([projectile], this.projectiles);
+          continue;
+        }
+      }
 
       if (projectile.timeState < 0) {
         this.landProjectile(projectile);
@@ -43,7 +65,7 @@ export class MapContext {
     const hitUnits = filterBySeekConditions(
       this.context.unit.units,
       ["enemy-team", "alive", ["in-distance", { distance: projectile.area }]],
-      { team: projectile.source.team, targetLocation: projectile.targetLocation },
+      { team: projectile.source.team, targetLocation: projectile.location },
     );
 
     hitUnits.forEach(unit => this.context.effect.applyEffect(projectile.effect, unit));
